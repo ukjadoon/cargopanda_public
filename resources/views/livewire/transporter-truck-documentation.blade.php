d5293b028a2777ac719e5ed22ed206407d388480
Umar Jadoon <umar@cinch.nu>
2020-08-09 22:46:51 +0300

    new menu links

--- resources/views/livewire/transporter-truck-documentation.blade.php
+++ resources/views/livewire/transporter-truck-documentation.blade.php
@@ -1,5 +1,5 @@
 <div>
-  <h2 class="text-gray-500 text-xs font-medium uppercase tracking-wide">Pinned Projects</h2>
+  <h2 class="text-gray-500 text-xs font-medium uppercase tracking-wide">Your trucks</h2>
   <ul class="mt-3 grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2 lg:grid-cols-4">
       @forelse($trucks as $truckDoc)
         <x-transporter.truck-info :truckDoc="$truckDoc"></x-transporter.truck-info>
