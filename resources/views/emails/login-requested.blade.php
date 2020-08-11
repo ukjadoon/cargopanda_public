a0f65c3356d582623b8e4658c66fc4887a7ec9e1
Umar Jadoon <umar@cinch.nu>
2020-08-11 11:26:28 +0300

    trying with new lines

--- resources/views/emails/login-requested.blade.php
+++ resources/views/emails/login-requested.blade.php
@@ -1,6 +1,6 @@
-Hello,
+Hello,<br><br>
 
-You can log into your Cargo Panda account by clicking <a href="{{ env('APP_URL') }}/authenticate/{{ $authToken->token }}" target="_blank">here</a>
+You can log into your Cargo Panda account by clicking <a href="{{ env('APP_URL') }}/authenticate/{{ $authToken->token }}" target="_blank">here</a><br><br>
 
-Regards,
+Regards,<br>
 The Cargo Panda Team
