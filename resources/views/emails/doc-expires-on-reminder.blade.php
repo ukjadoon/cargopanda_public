0e985919bcb0335c467c10042e480bbf4d7f0d4a
Umar Jadoon <umar@cinch.nu>
2020-08-11 17:49:31 +0300

    use isset

--- resources/views/emails/doc-expires-on-reminder.blade.php
+++ resources/views/emails/doc-expires-on-reminder.blade.php
@@ -2,21 +2,21 @@ Dear {{ $user->name }},<br><br>
 
 The following documents require your attention,<br><br>
 
-@if ($expiringDocs['organizationDocs'])
+@isset ($expiringDocs['organizationDocs'])
 
 @foreach($expiringDocs['organizationDocs'] as $expiresIn => $organizationDoc)
 <p>{{ $organizationDoc->name }} expiring <b>{{ $expiresIn == 'today' ? $expiresIn : 'in ' . $expiresIn }}</b> on {{ $organizationDoc->organizations->first()->pivot->expires_on }}</p>
 @endforeach
 
-@endif
+@endisset
 
-@if ($expiringDocs['truckDocs'])
+@isset($expiringDocs['truckDocs'])
 
 @foreach($expiringDocs['truckDocs'] as $expiresIn => $truckDoc)
 <p>The truck {{ $truckDoc->trucks->first()->name }} has a document {{ $truckDoc->name }} expiring <b>{{ $expiresIn == 'today' ? $expiresIn : 'in ' . $expiresIn }}</b> on {{ $truckDoc->trucks->first()->pivot->expires_on }}</p>
 @endforeach
 
-@endif
+@endisset
 
 <br><br>
 Yours sincerely,<br>
